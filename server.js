e065bdf78b9f7be6ebcadf01ea2f14d1903687e7
algonacci <algonacci1@gmail.com>
2022-09-02 09:36:19 +0700

    add logging middleware

--- server.js
+++ server.js
@@ -4,15 +4,40 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: "John Doe",
+  },
+  {
+    id: 1,
+    name: "Albert Einstein",
+  },
+];
+
 app.listen(PORT, () => {
   console.log(`🚀Server is running on port ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.send({
-    id: 1,
-    name: "John Doe",
-  });
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} - ${delta}ms`);
+});
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const friend = friends[id];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({ message: "Friend not found" });
+  }
 });
 
 app.get("/messages", (req, res) => {
